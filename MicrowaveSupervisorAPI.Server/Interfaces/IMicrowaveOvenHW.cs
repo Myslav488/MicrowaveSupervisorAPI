a2a7ccb4146ce3cc90bca5d4199ed3d44facdbc0
Przemyslaw Grabowski <grabowski.przemek168@gmail.com>
2025-08-10 14:15:27 +0200

    Add project files.

---
+++ MicrowaveSupervisorAPI.Server/Interfaces/IMicrowaveOvenHW.cs
@@ -0,0 +1,34 @@
+﻿namespace MicrowaveSupervisorAPI.Server.Interfaces
+{
+    /// <summary>
+    /// Interface to the Microwave oven hardware
+    /// </summary>
+    public interface IMicrowaveOvenHW
+    {
+        /// <summary>
+        /// Turns on the Microwave heater element
+        /// </summary>
+        void TurnOnHeater();
+
+        /// <summary>
+
+        /// Turns oƯ the Microwave heater element
+        /// </summary>
+        void TurnOƯHeater();
+
+        /// <summary>
+        /// Indicates if the door to the Microwave oven is open or closed
+        /// </summary>
+        bool DoorOpen { get; }
+
+        /// <summary>
+        /// Signal if the Door is opened or closed,
+        /// </summary>
+        event Action<bool> DoorOpenChanged;
+
+        /// <summary>
+        /// Signals that the Start button is pressed
+        /// </summary>
+        event EventHandler StartButtonPressed;
+    }
+}
